000d2d51e7f98815940b5eae3b4c93618b899868
Pedro Silva <pdx.henrique@gmail.com>
2021-09-18 20:12:55 -0300

    Initial commit

---
+++ app/src/main/java/one/digitalinnovation/coroutines/MainActivity.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

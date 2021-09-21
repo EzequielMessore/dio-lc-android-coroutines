b515daa5dbc918ba52565e8fcaa83df4bca7d972
Pedro Silva <pdx.henrique@gmail.com>
2021-09-21 19:21:23 -0300

    Add retrofit generator

---
+++ app/src/main/java/one/digitalinnovation/coroutines/data/api/RetrofitGenerator.kt
@@ -0,0 +1,29 @@
+package one.digitalinnovation.coroutines.data.api
+
+import com.squareup.moshi.Moshi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+
+object RetrofitGenerator {
+
+    private val moshi = Moshi.Builder()
+        .build()
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(
+            HttpLoggingInterceptor().setLevel(Level.BODY)
+        )
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://viacep.com.br/ws/")
+        .client(client)
+        .addConverterFactory(
+            MoshiConverterFactory.create(moshi)
+        )
+        .build()
+}
